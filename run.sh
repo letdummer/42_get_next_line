ba512ca7a86152bd59f7a9604f43be3d94fc3db4
letdummer <dummerleticia@gmail.com>
2025-06-11 09:43:40 +0100

    change name of the functions in gnl_utils because of libft

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-cc -Wall -Werror -Wextra main.c get_next_line.c get_next_line_utils.c -o program && ./program
+cc -Wall -Werror -Wextra main_GNL.c get_next_line.c get_next_line_utils.c -o program && ./program
 
 
 # move this line to between '&&' and './program'
